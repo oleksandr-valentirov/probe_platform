7dcc6d47fe26e9779d0f9395971fc8769a6fa776
Aleks Anatoliyovich <44201618+Aleks0010V@users.noreply.github.com>
2021-07-28 08:31:47 +0300

    clock init func progress

--- Drivers/SystemClock/clock.c
+++ Drivers/SystemClock/clock.c
@@ -25,29 +25,43 @@ uint8_t LSE_Init(void)
 uint8_t HSE_Init(void)
 {
     RCC_DeInit();  // тактируемся от HSI
-    uint16_t i = 0;
+    uint32_t i = 0;
     
     // запускаем HSE
     SET_BIT(RCC->CR, RCC_CR_HSEON | RCC_CR_HSEBYP);
-    while (i < 0xFFFF || !READ_BIT(RCC->CR, RCC_CR_HSERDY))
+    while (i < 0xFFFFFFFF || !READ_BIT(RCC->CR, RCC_CR_HSERDY))
         i++;
     // если не запустился
     if(!READ_BIT(RCC->CR, RCC_CR_HSERDY))
         return 1;
     
     // настраиваем PLL
+//    RCC_PLLConfig(RCC_PLLSource_HSE, 25, 200, 2);
     
     // запускаем PLL
     SET_BIT(RCC->CR, RCC_CR_PLLON); i = 0;
-    while (i < 0xFFFF || !READ_BIT(RCC->CR, RCC_CR_PLLRDY))
+    while (i < 0xFFFFFFFF || !READ_BIT(RCC->CR, RCC_CR_PLLRDY))
         i++;
     // если не запустился
     if(!READ_BIT(RCC->CR, RCC_CR_HSERDY))
         return 2;
     
-    // настраиваем скорость работы с памятью
+    // настраиваем скорость работы с памятью, питание и тактирование шин
+    PWR->CR |= PWR_CR_VOS;  // второй режим питания (100 MHz)
     
-    // переключаемся на HSE, настраиваем тактирование шин
+    SET_BIT(FLASH->ACR, FLASH_ACR_PRFTEN |     // предварительное извлечение инструкций из памяти
+        FLASH_ACR_ICEN |                // кеширование инструкций
+            FLASH_ACR_DCEN |            // кеширование памяти
+                FLASH_ACR_LATENCY_3WS);  // задержка в 4 цикла CPU
+    
+    // переключаемся на HSE
+    
     
     return 0;
 }
+
+
+
+
+
+
