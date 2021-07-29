ef6614283d8e69aaff259958a2a8989601a664b6
aleks valentirov <valentirov.o@ajax.systems>
2021-07-29 13:57:24 +0300

    SPI updates

--- Drivers/SPI/SPI.h
+++ Drivers/SPI/SPI.h
@@ -6,8 +6,8 @@
 
 /* ------------- LoRa functions ----------- */
 void SPI3_Init(void);
-void SPI3_send_data(uint16_t data);
-uint16_t SPI3_receive_data(void);
+uint8_t SPI3_send_data(uint16_t data);
+uint8_t SPI3_receive_data(uint16_t *dest);
 void SPI3_data_size_config(uint8_t size);
 /* ---------------------------------------- */
 
