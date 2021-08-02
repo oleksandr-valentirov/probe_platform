1661466757c8666d295df47c3f6a61bfc3a7c771
Aleks Anatoliyovich <44201618+Aleks0010V@users.noreply.github.com>
2021-08-02 21:53:49 +0300

    LoRa TXEN, RXEN, BUSY and NRESET pins init

--- Modules/SX1268/sx126x.c
+++ Modules/SX1268/sx126x.c
@@ -30,5 +30,31 @@ uint8_t LoRa_Init(void)
 
 void LoRa_PinsInit(void)
 {
+    GPIO_InitTypeDef pin;
+    
+    /* Output pins ---------------------------------------------------------- */ 
+    pin.GPIO_Pin = LORA_NRESET_PIN;
+    pin.GPIO_Speed = GPIO_Low_Speed;
+    pin.GPIO_OType = GPIO_OType_PP;
+    pin.GPIO_PuPd = GPIO_PuPd_UP;       // NRESET valid on low state
+    pin.GPIO_Mode = GPIO_Mode_OUT;
+    GPIO_Init(LORA_NRESET_PORT, &pin);
+    
+    pin.GPIO_PuPd = GPIO_PuPd_DOWN;     // TXEN valid on high state
+    pin.GPIO_Pin = LORA_TXEN_PIN;
+    GPIO_Init(LORA_TXEN_PORT, &pin);
+    
+    pin.GPIO_PuPd = GPIO_PuPd_DOWN;
+    pin.GPIO_Pin = LORA_RXEN_PIN;       // RXEN valid on high state
+    GPIO_Init(LORA_RXEN_PORT, &pin);
+
+    /* Input pins ----------------------------------------------------------- */ 
+    pin.GPIO_Pin = LORA_BUSY_PIN;
+    pin.GPIO_Mode = GPIO_Mode_IN;
+    pin.GPIO_PuPd = GPIO_PuPd_NOPULL;
+    GPIO_Init(LORA_BUSY_PORT, &pin);
     
 }
+
+
+
