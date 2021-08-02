0e73945b79a3dcabdf0c8316d3a4a45fc1a316db
Aleks Anatoliyovich <44201618+Aleks0010V@users.noreply.github.com>
2021-08-02 23:57:10 +0300

    LoRa pins comments

--- Modules/SX1268/sx126x.c
+++ Modules/SX1268/sx126x.c
@@ -33,25 +33,29 @@ void LoRa_PinsInit(void)
     GPIO_InitTypeDef pin;
     
     /* Output pins ---------------------------------------------------------- */ 
+    
+    // NRESET valid on low state
     pin.GPIO_Pin = LORA_NRESET_PIN;
     pin.GPIO_Speed = GPIO_Low_Speed;
     pin.GPIO_OType = GPIO_OType_PP;
-    pin.GPIO_PuPd = GPIO_PuPd_UP;       // NRESET valid on low state
+    pin.GPIO_PuPd = GPIO_PuPd_NOPULL;
     pin.GPIO_Mode = GPIO_Mode_OUT;
     GPIO_Init(LORA_NRESET_PORT, &pin);
     
-    pin.GPIO_PuPd = GPIO_PuPd_DOWN;     // TXEN valid on high state
+    // TXEN valid on high state
+    pin.GPIO_PuPd = GPIO_PuPd_NOPULL;
     pin.GPIO_Pin = LORA_TXEN_PIN;
     GPIO_Init(LORA_TXEN_PORT, &pin);
     
-    pin.GPIO_PuPd = GPIO_PuPd_DOWN;
-    pin.GPIO_Pin = LORA_RXEN_PIN;       // RXEN valid on high state
+     // RXEN valid on high state
+    pin.GPIO_PuPd = GPIO_PuPd_NOPULL;
+    pin.GPIO_Pin = LORA_RXEN_PIN;
     GPIO_Init(LORA_RXEN_PORT, &pin);
 
     /* Input pins ----------------------------------------------------------- */ 
     pin.GPIO_Pin = LORA_BUSY_PIN;
     pin.GPIO_Mode = GPIO_Mode_IN;
-    pin.GPIO_PuPd = GPIO_PuPd_NOPULL;
+    pin.GPIO_PuPd = GPIO_PuPd_DOWN;
     GPIO_Init(LORA_BUSY_PORT, &pin);
     
 }
