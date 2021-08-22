4bda9596b59b3dd16b2f1477ffe4fe6b1506b4ce
aleks valentirov <valentirov.o@ajax.systems>
2021-08-22 16:00:43 +0300

    onewire pin init fix

--- Projects_Library/F3/Modules/OneWire/onewire.c
+++ Projects_Library/F3/Modules/OneWire/onewire.c
@@ -28,7 +28,7 @@ void OneWire_Write_Pin_Init(void)
 {
     GPIO_InitTypeDef pin;
     pin.GPIO_Speed = GPIO_Speed_2MHz;
-    pin.GPIO_Mode = GPIO_Mode_Out_PP;
+    pin.GPIO_Mode = GPIO_Mode_Out_OD;
     pin.GPIO_Pin = ONEWIRE_PIN;
     GPIO_Init(ONEWIRE_PORT, &pin);
     SET_BIT(ONEWIRE_PORT->ODR, ONEWIRE_PIN);
@@ -39,7 +39,7 @@ void OneWrie_Read_Pin_Init(void)
 {
     GPIO_InitTypeDef pin;
     pin.GPIO_Speed = GPIO_Speed_2MHz;
-    pin.GPIO_Mode = GPIO_Mode_AF_PP;
+    pin.GPIO_Mode = GPIO_Mode_AF_OD;
     pin.GPIO_Pin = ONEWIRE_PIN;
     GPIO_Init(ONEWIRE_PORT, &pin);
     SET_BIT(ONEWIRE_PORT->ODR, ONEWIRE_PIN);
