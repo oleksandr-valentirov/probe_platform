8e601623e472e3a52e69b833f3b7af4b1aeba069
Aleks Anatoliyovich <44201618+Aleks0010V@users.noreply.github.com>
2021-08-28 10:31:02 +0300

    tim9 init

--- Projects_Library/F3/Drivers/timers/timers.c
+++ Projects_Library/F3/Drivers/timers/timers.c
@@ -10,6 +10,21 @@ typedef struct{
 /* TIM9 general functions --------------------------------------------------- */
 void TIM9_Init(void)
 {
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_TIM9, ENABLE);
+    
+    // basic init
+    TIM_TimeBaseInitTypeDef tim;
+    tim.TIM_Prescaler = SystemCoreClock / 1000000;  // 1 us == 1 count
+    tim.TIM_ClockDivision = TIM_CKD_DIV1;  // relates to filters
+    tim.TIM_CounterMode = TIM_CounterMode_Up;
+    tim.TIM_RepetitionCounter = 0;  // is used only in TIM1 and TIM 8
+    tim.TIM_Period = 0;
+    TIM_TimeBaseInit(TIM9, &tim);
+    
+    // CH 1 init to be 1-wire time-slot keeper
+    
+    
+    // CH 2 init to be transmitter/receiver
 }
 
 void TIM9_DeInit(void)
