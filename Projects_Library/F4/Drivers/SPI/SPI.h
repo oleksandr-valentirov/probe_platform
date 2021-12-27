fb5ff69371b785681b0cd872ac9f0e133fe5cb71
aleks anatoliyovich <aleks0010v@gmail.com>
2021-12-27 15:07:46 +0200

    gps branch fix v2

--- Projects_Library/F4/Drivers/SPI/SPI.h
+++ Projects_Library/F4/Drivers/SPI/SPI.h
@@ -8,11 +8,31 @@
 #define SPI_BUF_MASK            127
 
 #define SPI_FLAG_BUSY           1
+#define SPI_FLAG_RO             2
 
 
+typedef enum
+{
+    SPI_None = 0,
+    SPI_Flash = 1,
+    SPI_GPS = 2,
+} SPIDev_t;
+
+
+/*----------- Extern functions -----------*/
 void SPI3_Init(void);
+uint8_t SPI3_getc(uint8_t *dst);
+uint8_t SPI3_StartWriting(uint8_t *src, size_t size, SPIDev_t device);
+uint8_t SPI3_StartReading(size_t size, SPIDev_t device, uint8_t dummy_byte);
+/* Mutexes */
+uint8_t SPI3_SetMutex(GPIO_TypeDef *GPIOx, uint32_t GPIO_Pin, SPIDev_t device);
+void SPI3_FreeMutex(GPIO_TypeDef *GPIOx, uint32_t GPIO_Pin);
+uint8_t SPI3_IsMine(SPIDev_t device);
+uint8_t SPI3_IsFree(void);
+
+
+/* Interrupt routine */
 void SPI3_ExchangeBytes(void);
 void SPI3_gets(uint8_t *dst, uint8_t max_size);
-
 
 #endif
--- Projects_Library/F4/Modules/GPS/gps.c
+++ Projects_Library/F4/Modules/GPS/gps.c
@@ -1,7 +1,8 @@
 #include "gps.h"
 
+#define SPI_DEV_ID      SPI_GPS
+
 static char resp_buffer[GPS_RESP_BUF_SIZE] = {0};
-static char rd_buffer[GPS_RD_BUF_SIZE] = {0};
 static uint8_t wr_pos = 0;
 static uint8_t rd_pos = 0;
 
@@ -12,85 +13,81 @@ static void GPS_gets(void);
 static void GPS_ParseRMS(void);
 
 
-/* Flags */
+/* Flags ---------------------------------------------------------------------*/
 static uint8_t flags;
 
+#define SetNLFlag       SET_BIT(flags, GPS_FLAG_NL)
+#define GetNLFlag       READ_BIT(flags, GPS_FLAG_NL)
 
-void GPS_DataParser(void)
+/*----------------------------------------------------------------------------*/
+
+
+void GPS_main(void)
 {
-    GPS_gets();
-    
-    if (rd_buffer[1] == 'P')
-    {   /* proprietary message start */
+    if (SPI3_IsMine(SPI_DEV_ID)) 
+    {   /* read SPI buffer and start reception operation */
+        GPS_gets();
     }
-    else
-    {   /* standard message start */
-        switch (rd_buffer[3])
-        {
-        case 'G':
-            break;
-        case 'V':
-            break;
-        case 'T':
-            break;
-        case 'D':
-            break;
-        case 'R':
-            GPS_ParseRMS();
-            break;
-        case 'Z':
-            break;
+    if (SPI3_IsFree() && 0)
+    {   /* check that SPI is free and it is time to perform IO */
+    }
+    
+    if (GetNLFlag)
+    {
+        if (resp_buffer[1] == 'P')
+        {   /* proprietary message start */
+        }
+        else
+        {   /* standard message start */
+            switch (resp_buffer[3])
+            {
+            case 'G':
+                break;
+            case 'V':
+                break;
+            case 'T':
+                break;
+            case 'D':
+                break;
+            case 'R':
+                GPS_ParseRMS();
+                break;
+            case 'Z':
+                break;
+            }
         }
     }
 }
 
 
-void GPS_putc(uint8_t c)
-{
-    if (c == '\n')
-    {   /* packet data end */
-        SET_BIT(flags, GPS_FLAG_NL);
-    }
-    else if (c == '*')
-    {   /* checksum */
-    }
-    else if (c == '$')
-    {   /* packet data start */
-    }
-    resp_buffer[wr_pos++] = c;
-    wr_pos &= SIM_RESP_BUF_MASK;
-}
-
-
-uint8_t GPS_getc(void)
-{
-    uint8_t c = resp_buffer[rd_pos];
-    resp_buffer[rd_pos++] = 0;
-    rd_pos &= GPS_RESP_BUF_MASK;
-    return c;
-}
-
-
 static void GPS_gets(void)
 {
-    uint8_t i = 0;
-    for(i = 0; i < GPS_RD_BUF_SIZE; i++)
+    uint8_t c = 0;
+
+    while(rd_pos < GPS_RESP_BUF_SIZE && SPI3_getc(&c))
     {
-        rd_buffer[i] = Sim_getc();
-        if (rd_buffer[i] == '\n') {break;}
+        resp_buffer[rd_pos++] = c;
+        rd_pos &= GPS_RESP_BUF_MASK;
+        if (c == '\n')
+        {
+            SetNLFlag;
+            rd_pos = 0;
+            break;
+        }
+         else if (c == '*')
+        {   /* checksum */
+        }
+        else if (c == '$')
+        {   /* packet data start */
+        }
     }
-    for(; i < GPS_RD_BUF_SIZE; i++)
-    {
-        rd_buffer[i] = 0;
-    }
-    CLEAR_BIT(flags, GPS_FLAG_NL);
 }
 
 
 static void GPS_ParseRMS(void)
 {
     uint8_t i = 0, status = 1;
-    char *segment = strtok(rd_buffer, ",");
+    char *segment = strtok(resp_buffer, ",");
     
     while (segment != NULL && status)
     {
