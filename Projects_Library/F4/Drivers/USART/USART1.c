61b3d0ed18e79a7e8315147588fa97480d6c41cc
aleks anatoliyovich <aleks0010v@gmail.com>
2022-02-12 12:49:15 +0200

    usart1 for gps, usart2 for header preparations

--- Projects_Library/F4/Drivers/USART/USART1.c
+++ Projects_Library/F4/Drivers/USART/USART1.c
@@ -1,8 +1,8 @@
-#include "USART2.h"
+#include "USART1.h"
 
 #define FLAG_BUSY               1
 
-// #define CALLBACK_FUNC           
+#define CALLBACK_FUNC           &GPS_EndOfTransaction
 
 
 static struct usart_state {
@@ -11,7 +11,7 @@ static struct usart_state {
     size_t counter;
 } state;
 
-static uint8_t resp_buffer[USART2_BUF_SIZE] = {0};
+static uint8_t resp_buffer[USART1_BUF_SIZE] = {0};
 static uint8_t rd_pos = 0;
 static uint8_t wr_pos = 0;
 
@@ -23,22 +23,22 @@ static uint8_t flags;
 #define Reset_Busy_Flag       CLEAR_BIT(flags, FLAG_BUSY)
 #define Get_Busy_Flag         READ_BIT(flags, FLAG_BUSY)
 
-uint8_t USART2_Get_Busy_Flag(void)
+uint8_t USART1_Get_Busy_Flag(void)
 {
     return Get_Busy_Flag;
 }
 /* -------------------------------------------------------------------------- */
 
-void USART2_Init(void)
+void USART1_Init(void)
 {
-    USART_DeInit(USART2);
-    RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART2, ENABLE);
+    USART_DeInit(USART1);
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1, ENABLE);
     
     USART_InitTypeDef init;
     USART_StructInit(&init);
-    USART_Init(USART2, &init);
-    USART_ITConfig(USART2, USART_IT_RXNE, ENABLE);
-    USART_ITConfig(USART2, USART_IT_TC, ENABLE);
+    USART_Init(USART1, &init);
+    USART_ITConfig(USART1, USART_IT_RXNE, ENABLE);
+    USART_ITConfig(USART1, USART_IT_TC, ENABLE);
 
     // set callback
 #ifdef CALLBACK_FUNC
@@ -47,12 +47,12 @@ void USART2_Init(void)
     state.end_of_trancsaction_callback = NULL;
 #endif
     
-    USART_Cmd(USART2, ENABLE);
-    NVIC_EnableIRQ(USART2_IRQn);
+    USART_Cmd(USART1, ENABLE);
+    NVIC_EnableIRQ(USART1_IRQn);
 }
 
 
-uint8_t USART2_Start_Transmission(void* source, size_t counter)
+uint8_t USART1_Start_Transmission(void* source, size_t counter)
 {
     if (Get_Busy_Flag || source == NULL || counter == 0)
     {
@@ -63,7 +63,7 @@ uint8_t USART2_Start_Transmission(void* source, size_t counter)
     Set_Busy_Flag;
     
     /* platform logic */
-    USART2_Transmit_Next_Byte();
+    USART1_Transmit_Next_Byte();
     return 0;
 }
 
@@ -71,7 +71,7 @@ uint8_t USART2_Start_Transmission(void* source, size_t counter)
 /**
  * @brief       Returns next byte to interrupt routine
  */
-void USART2_Transmit_Next_Byte(void)
+void USART1_Transmit_Next_Byte(void)
 {
     if (state.ptr == NULL)
     {
@@ -81,7 +81,7 @@ void USART2_Transmit_Next_Byte(void)
     if (state.counter > 0)
     {
         state.counter--;
-        USART2->DR = *(state.ptr++);
+        USART1->DR = *(state.ptr++);
     }
     else
     {
@@ -95,20 +95,20 @@ void USART2_Transmit_Next_Byte(void)
 }
 
 /* IO -------------------------- */
-void USART2_putc(uint8_t c)
+void USART1_putc(uint8_t c)
 {
     resp_buffer[wr_pos++] = c;
-    wr_pos &= USART2_BUF_MASK;
+    wr_pos &= USART1_BUF_MASK;
 }
 
-uint8_t USART2_getc(uint8_t *ptr)
+uint8_t USART1_getc(uint8_t *ptr)
 {
     if (wr_pos == rd_pos)
     {
         return 0;
     }
     *ptr = resp_buffer[rd_pos++];
-    rd_pos &= USART2_BUF_MASK;
+    rd_pos &= USART1_BUF_MASK;
     return 1;
 }
 /* ---------------------------- */
