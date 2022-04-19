0e0b0e5c2728ac652eaa853d21c879f3b07be9a0
aleks anatoliyovich <aleks0010v@gmail.com>
2022-04-19 17:22:34 +0300

    gps structure update

--- Projects_Library/F4/Modules/GPS/m8.h
+++ Projects_Library/F4/Modules/GPS/m8.h
@@ -3,13 +3,14 @@
 #define __M8_H
 
 #include "!Project_library.h"
+#include "ubx.h"
 
 #define GPS_BUF_SIZE            128
 #define GPS_BUF_MASK            127
 
 /* external functions */
-//void GPS_Init(void);
-//void GPS_main(void);
-//void GPS_EndOfTransaction(void);
+void GPS_Init(void);
+void GPS_main(void);
+void GPS_EndOfTransaction(void);
 
 #endif
