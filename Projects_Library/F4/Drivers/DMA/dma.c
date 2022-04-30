25afd4208839218055e154a03a42fe12bff77b0e
Valentirov Aleksandr <aleks0010v@gmail.com>
2022-04-30 23:44:20 +0300

    dma ubx in

--- Projects_Library/F4/Drivers/DMA/dma.c
+++ Projects_Library/F4/Drivers/DMA/dma.c
@@ -17,7 +17,6 @@ void DMA_GPSoutInit(uint32_t* gps_buf_ptr)
     ch.DMA_MemoryInc = DMA_MemoryInc_Enable;
     /*------------------------------------------------------------------------*/
     
-    RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_DMA2, ENABLE);
     DMA_ITConfig(DMA2_Stream7, DMA_IT_TC, ENABLE);
     DMA_Init(DMA2_Stream7, &ch);
 }
@@ -31,3 +30,21 @@ void DMA_GPSoutTransfer(uint8_t size)
     USART_DMACmd(USART1, USART_DMAReq_Tx, ENABLE);
 }
 
+
+void DMA_GPSinInit(uint32_t* gps_buf_prt)
+{
+    DMA_InitTypeDef ch;
+    DMA_StructInit(&ch);
+    
+    /* my params */
+    ch.DMA_DIR = DMA_DIR_PeripheralToMemory;
+    ch.DMA_Channel = DMA_Channel_4;
+    ch.DMA_Memory0BaseAddr = (uint32_t)gps_buf_ptr;
+    ch.DMA_PeripheralBaseAddr = (uint32_t)(&(USART1->DR));
+    ch.DMA_MemoryInc = DMA_MemoryInc_Enable;
+    ch.DMA_Mode = DMA_Mode_Circular;
+    /*------------------------------------------------------------------------*/
+    
+    DMA_Init(DMA2_Stream5, &ch);
+    DMA_SetCurrDataCounter(DMA2_Stream5, GPS_BUF_SIZE);
+}
--- Projects_Library/F4/Drivers/DMA/dma.h
+++ Projects_Library/F4/Drivers/DMA/dma.h
@@ -3,4 +3,5 @@
 #include "!Project_library.h"
 
 void DMA_GPSoutInit(uint32_t* gps_buf_ptr);
+void DMA_GPSinInit(uint32_t* gps_buf_prt);
 void DMA_GPSoutTransfer(uint8_t size);
