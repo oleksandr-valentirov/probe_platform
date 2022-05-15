37483ad565621ea516d81a1b99712b311d1c4ed8
Valentirov Aleksandr <aleks0010v@gmail.com>
2022-05-15 17:58:21 +0300

    DMA SPI3 update

--- Projects_Library/F4/Drivers/DMA/DMA_SPI3.c
+++ Projects_Library/F4/Drivers/DMA/DMA_SPI3.c
@@ -30,7 +30,7 @@ uint8_t DMA_SPI3outTransfer(uint8_t* data_ptr, uint8_t size)
     DMA_ClearStatus(OUT);
     OUT->M0AR = (uint32_t)data_ptr;
     DMA_SetCurrDataCounter(OUT, size);
-    
+    DMA_Cmd(OUT, ENABLE);
     return 0;
 }
 
@@ -41,14 +41,14 @@ void DMA_SPI3inInit(void)
     DMA_StructInit(&ch);
     
     /* my params */
-    ch.DMA_DIR = DMA_DIR_MemoryToPeripheral;
+    ch.DMA_DIR = DMA_DIR_PeripheralToMemory;
     ch.DMA_Channel = DMA_Channel_0;
     ch.DMA_PeripheralBaseAddr = (uint32_t)(&(SPI3->DR));
     ch.DMA_MemoryInc = DMA_MemoryInc_Enable;
     /*------------------------------------------------------------------------*/
     
-    DMA_ITConfig(OUT, DMA_IT_TC, ENABLE);
-    DMA_Init(OUT, &ch);
+    DMA_ITConfig(IN, DMA_IT_TC, ENABLE);
+    DMA_Init(IN, &ch);
 }
 
 uint8_t DMA_SPI3inTransfer(uint8_t* dst_ptr, uint8_t size)
@@ -60,6 +60,6 @@ uint8_t DMA_SPI3inTransfer(uint8_t* dst_ptr, uint8_t size)
     DMA_ClearStatus(IN);
     IN->M0AR = (uint32_t)dst_ptr;
     DMA_SetCurrDataCounter(IN, size);
-    
-    return 1;
+    DMA_Cmd(IN, ENABLE);
+    return 0;
 }
